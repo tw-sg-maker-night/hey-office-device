7f007a7bd974cb908546bf5bdf99aacac835c9dd
Stephan Dowding <stephan.dowding@gmail.com>
2016-12-20 20:51:29 +0800

    connecting to google speech

--- src/index.js
+++ src/index.js
@@ -1,51 +1,93 @@
+require('dotenv').config();
+
 import record from 'node-record-lpcm16'
-import {Detector, Models} from 'snowboy'
+// import {Detector, Models} from 'snowboy'
 
-const models = new Models();
-
-models.add({
-  file: 'resources/snowboy.umdl',
-  sensitivity: '0.5',
-  hotwords : 'snowboy'
+let speech = require('@google-cloud/speech')({
+  projectId: process.env.GOOGLE_SPEECH_PROJECT_ID,
+  keyFilename: process.env.GOOGLE_SPEECH_KEYFILE
 });
 
-models.add({
-  file: 'resources/alexa.umdl',
-  sensitivity: '0.5',
-  hotwords : 'alexa'
-});
-
-models.add({
-  file: 'resources/HeyOffice.pmdl',
-  sensitivity: '0.5',
-  hotwords : 'hey office'
-});
-
-const detector = new Detector({
-  resource: "resources/common.res",
-  models: models,
-  audioGain: 2.0
-});
-
-detector.on('silence', function () {
-  console.log('silence');
-});
-
-detector.on('sound', function () {
-  console.log('sound');
-});
-
-detector.on('error', function () {
-  console.log('error');
-});
-
-detector.on('hotword', function (index, hotword) {
-  console.log('hotword', index, hotword);
-});
+// const models = new Models();
+//
+// models.add({
+//   file: 'resources/snowboy.umdl',
+//   sensitivity: '0.5',
+//   hotwords : 'snowboy'
+// });
+//
+// models.add({
+//   file: 'resources/alexa.umdl',
+//   sensitivity: '0.5',
+//   hotwords : 'alexa'
+// });
+//
+// models.add({
+//   file: 'resources/HeyOffice.pmdl',
+//   sensitivity: '0.45',
+//   hotwords : 'hey office'
+// });
+//
+// const detector = new Detector({
+//   resource: "resources/common.res",
+//   models: models,
+//   audioGain: 2.0
+// });
+//
+// detector.on('silence', function () {
+//   console.log('silence');
+// });
+//
+// detector.on('sound', function () {
+//   console.log('sound');
+// });
+//
+// detector.on('hotword', function (index, hotword) {
+//   console.log('hotword', index, hotword);
+// });
+//
+// detector.on('error', function () {
+//   console.log('error');
+// });
 
 const mic = record.start({
   threshold: 0,
-  verbose: true
+  sampleRate: 16000
 });
 
-mic.pipe(detector);
+// mic.pipe(detector);
+
+var request = {
+  config: {
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+  },
+  singleUtterance: false,
+  interimResults: true,
+  verbose: true
+};
+
+const recognizeStream = speech.createRecognizeStream(request)
+   .on('error', (error) => {
+     console.log("hmmmm...")
+     console.error(error);
+   })
+   .on('data', (data) => {
+     console.log('data!! =D');
+     console.log(data);
+    //  process.stdout.write(data.results)
+   });
+
+mic.pipe(recognizeStream);
+
+console.log("Google is theoretically listening... don't give away your secrets!!")
+
+//
+// speech.recognize('./resources/audio.raw', {
+//   encoding: 'LINEAR16',
+//   sampleRate: 16000
+// }, function(err, transcript) {
+//   // transcript = 'how old is the Brooklyn Bridge'
+//   if(err) console.error(err);
+//   else console.log(transcript);
+// });
