f5262470465ccedc283341cf42d36ce7fcfdf791
Stephan Dowding <stephan.dowding@gmail.com>
2017-03-22 21:40:38 +0800

    moving -y option

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -6,9 +6,9 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get -y upgrade
 
-sudo apt-get install -y libasound2-dev swig3.0 python-pyaudio python3-pyaudio sox libatlas-base-dev nodejs yarn
+sudo apt-get -y install libasound2-dev swig3.0 python-pyaudio python3-pyaudio sox libatlas-base-dev nodejs yarn
 
 cat > ~/.asoundrc << EOF
 pcm.!default {
