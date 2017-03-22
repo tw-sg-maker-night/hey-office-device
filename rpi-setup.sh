7378f1d6c8238a467b6dc254a9133505fd7e1132
Stephan Dowding <stephan.dowding@gmail.com>
2017-03-22 18:42:14 +0800

    created rpi-setup script

---
+++ rpi-setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install -y libasound2-dev swig3.0 python-pyaudio python3-pyaudio sox libatlas-base-dev nodejs yarn
+
+cat > ~/.asoundrc << EOF
+pcm.!default {
+  type asym
+   playback.pcm {
+     type plug
+     slave.pcm "hw:0,0"
+   }
+   capture.pcm {
+     type plug
+     slave.pcm "hw:1,0"
+   }
+}
+EOF
