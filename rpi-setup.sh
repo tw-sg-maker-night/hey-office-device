f428db17eabf309ab94ab29df662133d83fef425
Stephan Dowding <stephan.dowding@gmail.com>
2017-03-22 20:11:25 +0800

    adding -y flag to apt-get upgrade

--- rpi-setup.sh
+++ rpi-setup.sh
@@ -6,7 +6,7 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 sudo apt-get install -y libasound2-dev swig3.0 python-pyaudio python3-pyaudio sox libatlas-base-dev nodejs yarn
 
